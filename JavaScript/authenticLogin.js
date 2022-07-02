ea4560bd870ae6c6283a2e6eb87103d91e4175e5
Leonardo Sell <leosell@MacBook-Air-de-Leonardo.local>
2022-07-02 12:50:42 -0300

    Validacao de user logado

--- JavaScript/authenticLogin.js
+++ JavaScript/authenticLogin.js
@@ -1,21 +1,20 @@
 let login = document.getElementById('acess-login')
 let password = document.getElementById('acess-password')
-let loggedUser
+let loggedUser = 0
 
 const submit = () => {
     const lista_user = getLocalStorage()
     let valida = lista_user.filter(user => user.id == login.value)
     let validaPassword = lista_user.filter(user => user.password == password.value)
-
     if (valida.length == 0) {
         Swal.fire({
-            icon: "Error",
+            icon: 'error',
             title: "Algo de errado...",
             text: "Login / Senha Incorreto"
         })
     } else if (validaPassword.length == 0) {
         Swal.fire({
-            icon: "Error",
+            icon: 'error',
             title: "Algo de errado...",
             text: "Login / Senha Incorreto"
         })
@@ -23,10 +22,11 @@ const submit = () => {
 
     if (valida[0].id == login.value) {
         if (valida[0].password == password.value) {
-            loggedUser = lista_user.id
+            loggedUser = valida[0].id
+            alert(loggedUser)
             localStorage.setItem('loggedUser', (loggedUser))
-            login = ''
-            password = ''
+            login.value = ''
+            password.value = ''
             window.location.href = 'logado.html'
         }
     }
--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -1,11 +1,1 @@
-const openModal = () => document.getElementById('modal').classList.add('active')
 
-const closeModal = () => {
-    limparInputs()
-    document.getElementById('modal').classList.remove('active')
-}
-
-document.getElementById('open-modal').addEventListener('click', openModal)
-
-document.getElementById('modalClose').addEventListener('click', closeModal)
-document.getElementById('cancel-button').addEventListener('click', closeModal)
